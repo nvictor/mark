34207b8c36d3f94d63d58af42188036fbcf6f542
Victor Noagbodji <noagbodjivictor@gmail.com>
2020-04-19 16:52:29 -0400

    simplifies everything

--- background.js
+++ background.js
@@ -1,15 +1,5 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { "message": "clicked" });
+chrome.browserAction.onClicked.addListener(function () {
+    chrome.bookmarks.getChildren("2", function (nodes) {
+        chrome.tabs.create({ "url": nodes[Math.floor(Math.random() * nodes.length)].url });
     });
 });
-
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if (request.message === "random_bookmark") {
-            chrome.bookmarks.getChildren("2", function (nodes) {
-                chrome.tabs.create({ "url": nodes[Math.floor(Math.random() * nodes.length)].url });
-            });
-        }
-    }
-);
--- content.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if (request.message === "clicked") {
-            chrome.runtime.sendMessage({ "message": "random_bookmark" });
-        }
-    }
-);
